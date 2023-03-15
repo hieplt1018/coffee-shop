f3dc98d4e2b21630ce4488e641dd81df7c667010
hieplt1018 <hieplt1018@gmail.com>
2023-03-15 18:06:33 +0700

    create-error-handle-middleware

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const errorMiddleWare  = require('./middlewares/middleware.js');
 
 app.use(express.json());
 
 const products = require('./routes/product');
 app.use('/api/v1', products);
+app.use(errorMiddleWare);
 
 module.exports = app;
