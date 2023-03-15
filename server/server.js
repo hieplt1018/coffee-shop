b2b4b370c4d11a7b0ed461dcf28765f9abff4345
hieplt1018 <hieplt1018@gmail.com>
2023-03-15 20:25:17 +0700

    handle-unhandled-promise-rejection-api

--- server/server.js
+++ server/server.js
@@ -6,6 +6,14 @@ dotenv.config({ path: 'server/config/config.env' });
 
 connectDatabase();
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log(`ERROR: ${err.message}`);
+  console.log('Shutting down the server due to Unhandled Promise rejection');
+  server.close(() => {
+    process.exit(1);
+  });
 });
