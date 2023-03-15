908974f5d094cefae37ef1b99081c25b64ecfbf1
hieplt1018 <hieplt1018@gmail.com>
2023-03-15 17:40:19 +0700

    delete-a-product

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -49,10 +49,25 @@ exports.updateProduct = async (req, res, nest) => {
     runValidators: true
   });
 
-  console.log(req);
-;
   res.status(200).json({
     success: true,
     product
+  })
+}
+
+exports.deleteProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if(!product) {
+    return res.status(404).json({
+      success: false,
+      message: 'Product not found'
+    })
+  }
+  
+  await product.deleteOne();
+
+  res.status(200).json({
+    success: true,
+    message: 'Product is deleted!'
   })
 }
