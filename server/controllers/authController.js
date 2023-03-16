40415f0027c354fef545f3f69aa1276c0fca1303
hieplt1018 <hieplt1018@gmail.com>
2023-03-16 21:30:41 +0700

    [api]reset-new-password

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,6 +3,7 @@ const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middleware/catchAsyncErrors');
 const sendToken = require('../utils/jwtToken');
 const sendEmail = require('../utils/sendEmail');
+const crypto = require('crypto');
 
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
   const { name, email, password } = req.body;
@@ -51,7 +52,8 @@ exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
   await user.save({ validateBeforeSave: false });
 
   const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/reset/${resetToken}`;
-  const message = `Your password reset token is as follow:\n\n${resetUrl}\n\nIf you have not requested this email, the ignore it.`
+  const message = `Your password reset token is as follow:\n\n${resetUrl}\n\n
+    If you have not requested this email, the ignore it.`
 
   try {
     await sendEmail({
@@ -71,6 +73,30 @@ exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
     await user.save({ validateBeforeSave: false });
     return next(new ErrorHandler(error.message, 500));
   }
+});
+
+exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
+  const resetPasswordToken = crypto.createHash('sha256').update(req.params.token)
+    .digest('hex');
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() }
+  });
+
+  if(!user) {
+    return next(new ErrorHandler('Password reset token is invalid or has been expired'
+      , 400));
+  };
+  
+  if(req.body.password !== req.body.confirmPassword) {
+    return next(new ErrorHandler('Password does not match', 400));
+  };
+
+  user.password = req.body.password;
+  user.resetPasswordExpire = undefined;
+  user.resetPasswordToken = undefined;
+  await user.save();
+  sendToken(user, 200, res);
 });
 
 exports.logout = catchAsyncErrors( async (req, res, next) => {
