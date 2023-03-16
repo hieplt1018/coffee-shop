dd4b38123ca8a8f5dfa51aa160af7a151049ef2c
hieplt1018 <hieplt1018@gmail.com>
2023-03-16 11:06:36 +0700

    handle-validation-mogoose-id-error

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const errorMiddleWare  = require('./middleware/middleware.js');
+const errorMiddleWare  = require('./middleware/errors');
 
 app.use(express.json());
 
