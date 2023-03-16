b5e80dc1b775b3c59cfebbb65d8196f6ae414df5
hieplt1018 <hieplt1018@gmail.com>
2023-03-16 18:12:41 +0700

    [api]protect-routes-form-authenticate-users

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
 const errorMiddleWare  = require('./middleware/errors');
 
 app.use(express.json());
+app.use(cookieParser());
 
 const products = require('./routes/product');
 const auth = require('./routes/auth');
--- server/utils/jwtToken.js
+++ server/utils/jwtToken.js
@@ -1,3 +1,5 @@
+const User = require("../models/user");
+
 const sendToken = (user, statusCode, res) => {
   const token = user.getJwtToken();
 
