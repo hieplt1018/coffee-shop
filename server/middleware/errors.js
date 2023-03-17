7933f80cb26f2ac02b2f3663d5ae003085aed73f
hieplt1018 <hieplt1018@gmail.com>
2023-03-17 09:12:47 +0700

    [api]handle-wrong-JWT-Token-and-duplicate-id

--- server/middleware/errors.js
+++ server/middleware/errors.js
@@ -25,7 +25,21 @@ module.exports = (err, req, res, next) => {
     
     if (err.name === 'ValidationError') {
       const message = Object.values(err.errors).map(value => value.message);
-      console.log(err.errors)
+      error = new ErrorHandler(message, 400);
+    }
+
+    if (err.code = 11000) {
+      const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+      error = new ErrorHandler(message, 400);
+    }
+
+    if (err.name === 'JsonWebTokenError') {
+      const message = 'JSON Web Token is invalid. Try Again!!!';
+      error = new ErrorHandler(message, 400);
+    }
+
+    if (err.name === 'TokenExpiredError') {
+      const message = 'JSON Web Token is expired. Try Again!!!';
       error = new ErrorHandler(message, 400);
     }
 
