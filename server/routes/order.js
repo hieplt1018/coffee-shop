1f9ec26e0299d8536b5b4f8180f7b249dee39bb5
hieplt1018 <hieplt1018@gmail.com>
2023-03-17 21:00:06 +0700

    [api]update-process-order

--- server/routes/order.js
+++ server/routes/order.js
@@ -4,13 +4,18 @@ const { isAuthenticateUser, authorizeRoles } = require('../middleware/auth');
 const { 
   newOrder, 
   getMyOrders, 
-  getMySingleOrder
+  getMySingleOrder,
+  getAllOrders,
+  updateProcessOrder
 } = require('../controllers/orderController');
 
 router.route('/order/new').post(isAuthenticateUser, newOrder);
 router.route('/orders/me').get(isAuthenticateUser, getMyOrders);
 router.route('/order/me/:id').get(isAuthenticateUser, getMySingleOrder);
-
+router.route('/admin/orders')
+  .get(isAuthenticateUser, authorizeRoles('staff', 'admin'), getAllOrders);
+router.route('/admin/order/:id').put(isAuthenticateUser, 
+  authorizeRoles('staff','admin'), updateProcessOrder);
 
 
 module.exports = router;
