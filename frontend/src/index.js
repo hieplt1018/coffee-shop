f63166c43e7b3a4f0e14f6696eb8af706932ce70
hieplt1018 <hieplt1018@gmail.com>
2023-03-21 07:07:15 +0700

    [fe]fetch-all-products

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOMClient from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import './style.css';
+import { Provider } from 'react-redux'
+import store from './store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+const root = ReactDOMClient.createRoot(container);
 root.render(
-  <React.StrictMode>
+  <Provider store={store} >
     <App />
-  </React.StrictMode>
-);
 
+  </Provider>
+);
