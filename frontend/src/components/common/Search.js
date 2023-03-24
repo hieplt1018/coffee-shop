9369170d700fa4b5b6e77b3fb210399c604cb01e
hieplt1018 <hieplt1018@gmail.com>
2023-03-24 08:30:33 +0700

    [fe]fix-search-feature

--- frontend/src/components/common/Search.js
+++ frontend/src/components/common/Search.js
@@ -2,7 +2,7 @@ import React, { Fragment, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 
 const Search = () => {
-  const [keyword, setKeyword] = useState();
+  const [keyword, setKeyword] = useState('');
   let history = useNavigate();
 
   const submitHandler = (e) => {
@@ -13,7 +13,7 @@ const Search = () => {
       history('/');
     }
   };
-  console.log(history);
+
   return (
     <Fragment>
       <div className="shop__option">
@@ -22,10 +22,11 @@ const Search = () => {
             <div className="shop__option__search">
               <form onSubmit={submitHandler} >
                 <input 
-                  type="text" 
-                  value={
+                  type="text"
+                  placeholder={
                     window.location.href.split('/').includes('products') ?
-                      'Search' : window.location.href.split('/').pop() }
+                    "Tìm kiếm" : window.location.href.split('/').pop() }
+                  value={keyword}
                   onChange={(e) => setKeyword(e.target.value)}
                 />
                 <button type="submit"><i className="fa fa-search" /></button>
