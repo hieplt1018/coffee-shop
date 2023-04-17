2fa8ba7c4803307eef0c68c97786548c49efbbaa
hieplt1018 <hieplt1018@gmail.com>
2023-04-17 08:31:25 +0700

    fix-small-bug

--- server/utils/jwtToken.js
+++ server/utils/jwtToken.js
@@ -7,7 +7,9 @@ const sendToken = (user, statusCode, res) => {
     exprires: new Date(
       Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true
+    httpOnly: true,
+    secure: 'auto',
+    sameSite: 'none'
   };
 
   res.status(statusCode).cookie('token', token, options).json({
